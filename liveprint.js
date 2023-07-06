a371d07f67b3db4bb245ac5f37c1dabd0d375b00
Marco Morales <marco.mp@qrsof.com>
2023-07-06 00:38:29 -0600

    environment1

--- liveprint.js
+++ liveprint.js
@@ -5318,7 +5318,7 @@ addEventListener("submit", async (event) => {
 
 async function saveRecord(requestData) {
     console.log(`saveRecord#clientToken:#${clientToken}#`)
-    const response = await fetch(`https://delightful-idea-t4wmj.ampt.app/liveprint/recording?clientToken=${clientToken}`, {
+    const response = await fetch(`https://sleek-source-xc6xk.ampt.app/liveprint/recording?clientToken=${clientToken}`, {
         method: 'POST',
         body: requestData,
         headers: {
