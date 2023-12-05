b6ada27dea29e16f51dbba9b0979887d3b004d9e
Marco Morales <marco.mp@qrsof.com>
2023-12-05 12:34:36 -0600

    staging

--- dist/formproof-concat.js
+++ dist/formproof-concat.js
@@ -8099,7 +8099,7 @@ const events = [];
 const storageRecord = 'FORMPROOF_EVENTS';
 let pathNamePage = window.location.pathname;
 let eventsToSave = {};
-const formProofApiSave = 'http://localhost:3000/api/public/formproof/saveRecord'
+const formProofApiSave = 'https://striking-idea-mrrn3.ampt.app/api/public/formproof/saveRecord'
 let savingLoading = false;
 let record = true;
 
