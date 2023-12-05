43144809c6a827b04df711e563d7ef8ab347784f
Marco Morales <marco.mp@qrsof.com>
2023-12-05 12:05:16 -0600

    change api

--- src/formproof.js
+++ src/formproof.js
@@ -18,7 +18,7 @@ const events = [];
 const storageRecord = 'FORMPROOF_EVENTS';
 let pathNamePage = window.location.pathname;
 let eventsToSave = {};
-const formProofApiSave = 'http://localhost:3000/api/public/formproof/saveRecord'
+const formProofApiSave = 'https://sweet-source-295d3.ampt.app/api/public/formproof/saveRecord'
 let savingLoading = false;
 let record = true;
 
