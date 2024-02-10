8237baea3d40bc92db7ae92721a659a411af002b
Marco Morales <marco.mp@qrsof.com>
2024-02-09 19:09:54 -0600

    test github action

--- gulpfile.js
+++ gulpfile.js
@@ -9,11 +9,42 @@ function installDependencies(cb) {
     cb();
 }
 
+const formProofEnvironmentApis = {
+    staging: 'https://bright-source-jxr9r.ampt.app/api',
+    production: 'https://intelligent-src-r12j9.ampt.app/api'
+};
+
+
+
+
 function build(cb) {
     console.log('Build')
     gulp.src(['node_modules/rrweb/dist/rrweb.js', 'src/formproof.js', 'src/tfaValidation.js', 'src/saveRecording.js', 'src/blackListPhone.js', 'src/utils/send2faCode.js', "src/utils/validate2faCode.js",
-    "src/utils/verifyPhoneBlackListApi.js", "src/utils/saveRecordings.js"])
+        "src/utils/verifyPhoneBlackListApi.js", "src/utils/saveRecordings.js"])
         .pipe(gp_concat('formproof-concat.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(gp_rename('formproof.js'))
+        .pipe(gp_uglify())
+        .pipe(gulp.dest('dist'));
+    cb();
+}
+function buildStaging(cb) {
+    gulp.src(['node_modules/rrweb/dist/rrweb.js', 'src/formproof.js', 'src/tfaValidation.js', 'src/saveRecording.js', 'src/blackListPhone.js', 'src/utils/send2faCode.js', "src/utils/validate2faCode.js",
+        "src/utils/verifyPhoneBlackListApi.js", "src/utils/saveRecordings.js"])
+        .pipe(gp_concat('formproof-concat.js'))
+        .pipe(replace(/let baseApi = '.*?';/, `let baseApi = '${formProofEnvironmentApis['staging']}';`)) // Replace baseApi based on environment
+        .pipe(gulp.dest('dist'))
+        .pipe(gp_rename('formproof.js'))
+        .pipe(gp_uglify())
+        .pipe(gulp.dest('dist'));
+    cb();
+}
+
+function buildProduction(cb) {
+    gulp.src(['node_modules/rrweb/dist/rrweb.js', 'src/formproof.js', 'src/tfaValidation.js', 'src/saveRecording.js', 'src/blackListPhone.js', 'src/utils/send2faCode.js', "src/utils/validate2faCode.js",
+        "src/utils/verifyPhoneBlackListApi.js", "src/utils/saveRecordings.js"])
+        .pipe(gp_concat('formproof-concat.js'))
+        .pipe(replace(/let baseApi = '.*?';/, `let baseApi = '${formProofEnvironmentApis['production']}';`)) // Replace baseApi based on environment
         .pipe(gulp.dest('dist'))
         .pipe(gp_rename('formproof.js'))
         .pipe(gp_uglify())
